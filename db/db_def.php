72bd8f0ec30def8f2eb7cc0ea689e854bb48bbe0
b8p31102 <64205746+b8p31102@users.noreply.github.com>
2021-03-08 09:24:22 +0900

    Add files via upload

---
+++ db/db_def.php
@@ -0,0 +1,8 @@
+<?php
+
+	$dbhost = "localhost";
+	$dbname = "cookpad_data";	
+        $dbuser = "b8p31102";
+	$dbpasswd = "B8P31102.pass";
+
+?>
---
+++ db/dbp_def.php
@@ -0,0 +1,9 @@
+<?php
+
+	$dbphost = "localhost";
+	$dbpname = "b8p31042";	
+        $dbpuser = "b8p31042";
+	$dbppasswd = "B8P31042.pass";
+    $dbptable = "nutrium";
+
+?>
